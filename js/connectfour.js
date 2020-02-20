c56b9a80858d094e49698bb0fd890b09a4728ae3
Sebastian Joseph <SJoseph@ucvts.tec.nj.us>
2020-02-20 13:21:33 -0500

    idk

--- js/connectfour.js
+++ js/connectfour.js
@@ -1,114 +1,70 @@
-let player1 = prompt("Player One: Enter Your Name , you will be Blue");
-let player1color = 'rgb(86, 151, 255)';
+///////////////////// CONSTANTS /////////////////////////////////////
+const winningConditions = [
+];
+///////////////////// APP STATE (VARIABLES) /////////////////////////
+let board;
+let turn;
+let win;
+///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+const squares = Array.from(document.querySelectorAll("#board div"));
+const message = document.querySelector("h2");
+///////////////////// EVENT LISTENERS ///////////////////////////////
+window.onload = init;
+document.getElementById("board").onclick = takeTurn;
+document.getElementById("reset-button").onclick = init;
+///////////////////// FUNCTIONS /////////////////////////////////////
+function init() {
+  board = [
+    "", "", "", "", "",""
+    "", "", "", "", "",""
+    "", "", "", "", "",""
+    "", "", "", "", "",""
+    "", "", "", "", "",""
+    "", "", "", "", "",""
+    "", "", "", "", "",""
+  ];
+  turn = "X";
+  win = null;
+  render();
+};
 
-let player2 = prompt("Player Two: Enter Your Name, you will be Red");
-let player2color = 'rgb(237, 45, 73)';
 
-let gameon = true;
-let table = $('table tr');
+function render() {
+  board.forEach(function(mark, index) {
+    squares[index].textContent = mark;
+  });
+  message.textContent = win === "T" ? "It's a tie!" : win ? `${win} wins!` : `Turn: ${turn}`;
 
-function reportWin(rowNum, colNum) {
-  console.log("You won starting at this row, col");
-  console.log(rowNum);
-  console.log(colNum);
 }
+function takeTurn(e) {
+  if (!win) {
+  let index = squares.findIndex(function(square){
+    return square === e.target
+  });
 
-function changeColor(rowIndex, colIndex, color) {
-  return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color', color);
-}
-
-function returnColor(rowIndex, colIndex, color) {
-  return  table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color');
-}
-
-function checkBottom(colIndex) {
-  let colorReport = returnColor(5, colIndex);
-  for (let row = 5; row > -1; row--) {
-    colorReport = returnColor(row, colIndex);
-    if (colorReport === 'rgb(128, 128, 128)') {
-      return row
+if (board[index] === "") {
+  board[index] = turn;
+  turn = turn === "red" ? "black" : "red";
+  win = getWinner();
+  render();
     }
   }
 }
 
-function colorMatchCheck(one, two, three, four) {
-  return (one === two && one === three && one === four && one != 'rgb(128, 128, 128)' && one !== undefined);
-}
+function getWinner() {
+  let winner = null;
 
-function horizontalWinCheck() {
-  for (let row = 0; row < 6; row++) {
-    for (let col = 0; col < 4; col++) {
-      if (colorMatchCheck(returnColor(row, col), returnColor(row, col+1), returnColor(row, col+2), returnColor(row, col+3))) {
-        console.log('horiz');
-        reportWin(row, col);
-        return true;
-      } else {
-        continue;
-      }
+  winningConditions.forEach(function(condition, index) {
+    if (
+    ) {
     }
-  }
-}
+  });
 
-function verticalWinCheck() {
-  for (let col = 0; col < 7; col++) {
-    for (let row = 0; row < 3; row++) {
-      if (colorMatchCheck(returnColor(row, col), returnColor(row+1, col), returnColor(row+2, col), returnColor(row+3, col))) {
-        console.log('vertical');
-        reportWin(row, col);
-        return true;
-      } else {
-        continue;
-      }
-    }
-  }
-}
 
-function diagonalWinCheck() {
-  for (let col = 0; col < 5; col++) {
-    for (let row = 0; row < 7; row++) {
-      if (colorMatchCheck(returnColor(row, col), returnColor(row-1, col+1), returnColor(row-2, col+2), returnColor(row-3, col+3))) {
-        console.log('diag');
-        reportWin(row, col);
-        return true;
-      } else {
-        continue;
-      }
-    }
-  }
-}
 
-function end(winningPlayer) {
-  for (let col = 0; col < 7; col++) {
-    for (let row = 0; row < 7; row++) {
-      $('h1').text(winningPlayer+" has won! Refresh your browser to play again").css("fontSize", "50px")
-    }
-  }
-}
 
-let currentPlayer = 1;
-let currentName = player1;
-let currentColor = player1Color;
+  return winner ? winner : board.includes("") ? null : "T";
 
-$('h3').text(player1+": it is your turn");
-$('.board button').on('click', function() {
-  let col = $(this).closest("td").index();
-  let bottomAvail = checkBottom(col);
-  changeColor(bottomAvail, col, currentColor);
 
-if (horizontalWinCheck() || verticalWin() || diagonalWinCheck()) {
-  end(currentName);
 
 }
-
-currentPlayer = currentPlayer * -1;
-
-if (currentPlayer === 1) {
-  currentName = player1;
-  $('h3').text(currentName+": it is your turn");
-  currentColr = player1Color;
-} else {
-  currentName = player2;
-  $('h3').text(currentName+": it is your turn");
-  currentColor = player2Color;
-}
-})
