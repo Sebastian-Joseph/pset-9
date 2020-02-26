ee8e3509a174a5c786f8faec75110158d3bf3ead
Sebastian Joseph <theclimax25@gmail.com>
2020-02-25 22:38:44 -0500

    PROGRESS

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -1,7 +1,7 @@
 	var canvas = document.getElementById("myCanvas");
 	var ctx = canvas.getContext("2d");
 	var ballRadius = 10;
-	var ballColor = "#0095DD"
+	var ballColor = "black"
 	var speed = 10;
 	var x = canvas.width/2;
 	var y = canvas.height-30;
@@ -14,11 +14,9 @@
 	var paddleWidth = 75;
 	var paddleX = (canvas.width-paddleWidth)/2;
 
-	//Pressione tasti
-	var rightPressed = false;
+	va rightPressed = false;
 	var leftPressed = false;
 
-	//Mattoni
 	var brickRowCount = 3;
 	var brickColumnCount = 5;
 	var brickWidth = 75;
@@ -26,7 +24,7 @@
 	var brickPadding = 10;
 	var brickOffsetTop = 30;
 	var brickOffsetLeft = 30;
-	var brickColor = "#0095DD";
+	var brickColor = "black";
 
 	var bricks = [];
 	for(c = 0; c < brickColumnCount; c++) {
@@ -38,7 +36,7 @@
 
 	var score = 0;
 	var scoreFont = "16px Arial";
-	var scoreFillStyle = "#0095DD";
+	var scoreFillStyle = "black";
 
 	document.addEventListener("keydown", keyDownHandler, false);
 	document.addEventListener("keyup", keyUpHandler, false);
@@ -55,32 +53,28 @@
 	function drawBall() {
 		ctx.beginPath();
 		ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    0, altri dati
 		ctx.fillStyle = ballColor;
-		ctx.fill();
-		ctx.closePath();																	
-	}
-
-	//Disegno il Paddle
-	function drawPaddle() {
-		ctx.beginPath();
-		ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-		ctx.fillStyle = "#0095DD";
 		ctx.fill();
 		ctx.closePath();
 	}
 
-	//Disegno i Mattoni
+	function drawPaddle() {
+		ctx.beginPath();
+		ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+		ctx.fillStyle = "black";
+		ctx.fill();
+		ctx.closePath();
+	}
+
 	function drawBricks() {
 		for(c = 0; c < brickColumnCount; c++) {
 			for(r = 0; r < brickRowCount; r++) {
-				if(bricks[c][r].status == 1) {												//Calcolo posizione in base alle dimensioni
+				if(bricks[c][r].status == 1) {
 					var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
 					var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-					bricks[c][r].x = brickX;												//Assegno la posizione
+					bricks[c][r].x = brickX;
 					bricks[c][r].y = brickY;
 
-					//Disegno
 					ctx.beginPath();
 					ctx.rect(brickX, brickY, brickWidth, brickHeight);
 					ctx.fillStyle = brickColor;
@@ -91,7 +85,6 @@
 		}
 	}
 
-	//Ascolto Tasti
 	function keyDownHandler(e) {
 		if(e.keyCode == 39) {
 			rightPressed = true;
@@ -110,19 +103,17 @@
 		}
 	}
 
-	//Individuazione collisioni
 	function collisionDetection() {
 		for(c=0; c<brickColumnCount; c++) {
 			for(r=0; r<brickRowCount; r++) {
 				var b = bricks[c][r];
-				if(b.status == 1) {																	//Controllo che il mattone esista
-					if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {			//Controllo se la palla è "entrata" nell'area del mattone
-						dy = -dy;																	//Rimbalzo
-						b.status = 0;																//Cancello il mattone
-						ballColor = getRandomColor();												//Cambio colore
-						score++;																	//Aumento Punteggio
+				if(b.status == 1) {
+					if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+						dy = -dy;
+						b.status = 0;
+						ballColor = getRandomColor();
+						score++;
 
-						//Messaggio di vittoria
 						if(score == brickRowCount*brickColumnCount) {
 							alert("YOU WIN, CONGRATULATIONS!");
 							document.location.reload();
@@ -133,48 +124,39 @@
 		}
 	}
 
-	//Stampa Punteggio
 	function drawScore() {
 		ctx.font = scoreFont;
 		ctx.fillStyle = scoreFillStyle;
 		ctx.fillText("Score: "+score, 8, 20);
 	}
 
-	//Disegno l'intera pagina
 	function draw() {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);						//Pulisce dai rettangoli
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-		//Disegno le forme
 		drawBricks();
 		drawBall();
 		drawPaddle();
-		//Attivo individuazione collisioni
 		collisionDetection();
-		//Stampo punteggio
 		drawScore();
 
-		//Modifico variabili globali posizione
 		x += dx;
 		y += dy;
 
-		//Rimbalzo
-		if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {									//Rim
+		if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
 			dx = -dx;
-			ballColor = getRandomColor();																//Cambio colore
+			ballColor = getRandomColor();
 		}
 		if(y + dy < ballRadius) {
 			dy = -dy;
 		} else if(y + dy > canvas.height-ballRadius) {
-			if(x > paddleX && x < paddleX + paddleWidth) {												//Rimbalzo sul muro superiore o il Paddle
+			if(x > paddleX && x < paddleX + paddleWidth) {
 				dy = -dy;
 			}
-			else {																						//Game Over
-				alert("GAME OVER");
+			else {
 				document.location.reload();
 			}
 		}
 
-		//Rimbalzo Paddle
 		if(rightPressed && paddleX < canvas.width-paddleWidth) {
 			paddleX += px;
 		}
@@ -182,7 +164,6 @@
 			paddleX -= px;
 		}
 
-		//Game Over
 		if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
 			dx = -dx;
 		}
@@ -192,5 +173,4 @@
 		}
 	}
 
-	//Loop di draw()
 	setInterval(draw, speed);
